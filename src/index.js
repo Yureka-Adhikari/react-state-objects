352c7772158833424c72482867dc1d81754ea6d6
Yureka Adhikari <adhikariyureka@gmail.com>
2024-10-24 19:10:37 +0545

    yayyayayayay

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+class Student extends React.Component {
+  constructor(props) {
+    super(props);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+    this.state = {
+      name: "Yureka Adhikari",
+      grade: 8,
+      favorite_colour: "Purple",
+      favorite_sub: "English",
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> Student Details </h1>
+        <p> My name is {this.state.name} </p>
+        <p> I am in grade {this.state.grade} </p>
+        <p> My favourite colour is {this.state.favorite_colour} </p>
+        <p> My favourite subject is {this.state.favorite_sub} </p>
+      </div>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <Student />
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
